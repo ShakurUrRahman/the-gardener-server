cea8fd7e87f9db37ad0bd8ecdd1a959c7e3f6a1e
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-11-10 00:46:30 +0600

    showing service done

--- index.js
+++ index.js
@@ -9,15 +9,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
 
-
-const uri = "mongodb+srv://emaJohnDbUser:<password>@cluster0.stqqfzx.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.stqqfzx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('theGardener').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/allServices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => (console.error(err)))
 
 
 app.get('/', (req, res) => {
